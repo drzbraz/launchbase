a3584c124c092dc549490c75c960e9879d229363
Daniel Braz <drz.braz@gmail.com>
2020-06-26 16:37:30 -0300

    first commit

---
+++ data.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    id: 'LtdCt4QbQzk',
+    title: 'Trabalho AED - Algoritmos de Ordenação',
+    duration: '13m57s',
+    button: 'Play',
+  },
+  {
+    id: 'uhHoicg5xoM',
+    title: 'Como criar uma calculadora na linguagem Assembly',
+    duration: '4m',
+    button: 'Play',
+  },
+  {
+    id: 'EUJeiuwn660',
+    title: 'Projeto de Extensão Beneficente',
+    duration: '7m34s',
+    button: 'Play',
+  },
+]
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express()
+const videos = require('./data')
+
+server.use(express.static('public'))
+server.set('view engine', 'njk')
+
+nunjucks.configure('views', {
+  express: server,
+})
+
+server.get('/', function (req, res) {
+  return res.render('about')
+})
+
+server.get('/projetos', function (req, res) {
+  return res.render('projetos', { items: videos })
+})
+
+server.listen(5000, function () {
+  console.log('server is running')
+})
